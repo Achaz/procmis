7ba3423659fd93d63049a796d32bbe8815671ca9
Achaz <jtugume123@gmail.com>
2023-09-28 19:28:07 +0300

    cleaned up the invitations page

--- resources/views/auth/request.blade.php
+++ resources/views/auth/request.blade.php
@@ -1,54 +1,62 @@
-@php
-$isMenu = false;
-$navbarHideToggle = false;
-@endphp
 
-@extends('layouts/blankLayout')
+@extends('layouts/contentNavbarLayout')
 
-@section('title', 'Registration')
+@section('title', 'Invite User')
+
+@section('vendor-style')
+<link rel="stylesheet" href="{{asset('assets/vendor/libs/apex-charts/apex-charts.css')}}">
+@endsection
+
+@section('vendor-script')
+<script src="{{asset('assets/vendor/libs/apex-charts/apexcharts.js')}}"></script>
+@endsection
+
+@section('page-script')
+<script src="{{asset('assets/js/dashboards-analytics.js')}}"></script>
+@endsection
 
 @section('content')
-<div class="layout-demo-wrapper">
-@if (session('error'))
-<div class="alert alert-danger">
-    <p>{{ session('error') }}</p>
-</div>
-@endif
-
-@if (session('success'))
-<div class="alert alert-success">
-    <p>{{ session('success') }}</p>
-</div>
-@endif
-<form class="mb-3" method="POST" action="{{ route('storeInvitation') }}">
-        
-@csrf
-        <div class="mb-3 {{ $errors->has('email') ? ' has-error' : '' }}">
-            <label for="email" class="mb-3 control-label">E-Mail Address</label>
-
-            <div class="mb-3">
-                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                @if ($errors->has('email'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
-                @endif
+<div class="row">
+  <div class="col-12 col-lg-8 order-2 order-md-3 order-lg-2 mb-4">
+    <div class="card">
+      <div class="row row-bordered g-0">
+        <div class="col-md-6">
+           <div class="card-body">
+            <div class="layout-demo-wrapper">
+            @if (session('error'))
+            <div class="alert alert-danger">
+                <p>{{ session('error') }}</p>
             </div>
+            @endif
+
+            @if (session('success'))
+            <div class="alert alert-success">
+                <p>{{ session('success') }}</p>
+            </div>
+            @endif
+            <form class="mb-3" method="POST" action="{{ route('storeInvitation') }}">               
+                @csrf
+                <div class="mb-3 {{ $errors->has('email') ? ' has-error' : '' }}">
+                <label for="email" class="mb-3 control-label">E-Mail Address</label>
+                    <div class="mb-3">
+                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>     
+                <div class="mb-3">
+                <button type="submit" class="btn btn-primary">Send an invitation</button>
+                </div>       
+            </form>
+            </div>
+         </div>
         </div>
-
-        
-            <div class="mb-3">
-                <button type="submit" class="btn btn-primary">
-                    Request An Invitation
-                </button>
-            </div>
-            <div class="mb-3">
-                <a class="btn btn-link" href="{{ route('login.show') }}">
-                    Already Have An Account?
-                </a>
-            </div>
-        
-    </form>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
