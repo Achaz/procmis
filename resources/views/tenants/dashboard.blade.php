2452a546506a9175c18ddd685e88cd9d912f67d5
Joseph M. Matembu <jmatembu@users.noreply.github.com>
2023-09-30 14:52:05 +0300

    Show sample dashboard card

--- resources/views/tenants/dashboard.blade.php
+++ resources/views/tenants/dashboard.blade.php
@@ -5,11 +5,11 @@
 @section('content')
 <div class="row">
   <!-- Total Revenue -->
-  <div class="col-16 col-lg-12 order-2 order-md-3 order-lg-2 mb-4">
+  <div class="col-16 col-lg-3 order-2 order-md-3 order-lg-2 mb-4">
     <div class="card">
-      <div class="card-body">
-        <h3>Suppliers</h3>
-        <span>0</span>
+      <div class="card-body text-center">
+        <h1>0</h1>
+        <span>Suppliers</span>
       </div>
     </div>
   </div>
