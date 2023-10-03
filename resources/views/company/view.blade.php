4a7ce6385ca081ffa4253ef8573c9962451035cb
Achaz <jtugume123@gmail.com>
2023-10-03 11:04:14 +0300

    improved on the company user profile display page

--- resources/views/company/view.blade.php
+++ resources/views/company/view.blade.php
@@ -5,53 +5,117 @@
 @section('content')
 <div class="row">
   <div class="col-16 col-lg-12 order-2 order-md-3 order-lg-2 mb-4">
-    <div class="card">
-      <div class="row row-bordered g-0">
-        <div class="col-md-14">
-            <div class="card-body">
-                <div class="table-responsive text-nowrap">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Company Name</th>
-                                <th>Category</th>
-                                <th>Description</th>
-                                <th>Phone</th>
-                                <th>Country</th>
-                                <th>Registration Number</th>
-                                <th>TaxID</th>
-                                <th>Address</th>
-                                <th>City</th>
-                                <th>Region</th>
-                                <th>Zip Code</th>
-                            
-                            </tr>
-                        </thead>
-                        <tbody>                               
-                            <tr>
-                                <th scope="row">{{ $company->id }}</th>
-                                <td>{{ $company->organisationName }}</td>
-                                <td>{{ $company->procurementCategory }}</td>
-                                <td>{{ $company->briefDescription }}</td>
-                                <td>{{ $company->companyPhoneNumber }}</td>
-                                <td>{{ $company->country }}</td>
-                                <td>{{ $company->registrationNumber }}</td>
-                                <td>{{ $company->taxId }}</td>
-                                <td>{{ $company->address }}</td>
-                                <td>{{ $company->city }}</td>
-                                <td>{{ $company->region }}</td>
-                                <td>{{ $company->zip_code }}</td>
-                                <td><a href="{{ route('tenants.profile.edit', [tenant('id'), $company->id]) }}"
-                                        class="btn btn-info btn-sm">Edit</a></td>                                   
-                            </tr>                             
-                        </tbody>
-                    </table>
-                </div>               
+      <div class="col-md-8">
+        <div class="card mb-3">
+          <div class="card-body">
+          <h4>Your company Profile</h4>
+          <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Company Name</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->organisationName }}
+              </div>
             </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Category</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->procurementCategory }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Description</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->briefDescription }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Phone</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+                {{ $company->companyPhoneNumber }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Country</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->country }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Registration Number</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->registrationNumber }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">TaxID</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->taxId }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Address</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->address }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">City</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->city }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Region</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->region }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Zip Code</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->zip_code }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-12">
+                <a class="btn btn-info" href="{{ route('tenants.profile.edit', [tenant('id'), $company->id]) }}">Edit</a>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
+    </div>  
 </div>
 @endsection
