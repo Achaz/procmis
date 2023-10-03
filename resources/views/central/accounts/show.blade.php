d49aa43398ce15f1b1f4e317333d174467cacaa8
Achaz <jtugume123@gmail.com>
2023-10-03 12:15:11 +0300

    cleaning up

--- resources/views/central/accounts/show.blade.php
+++ resources/views/central/accounts/show.blade.php
@@ -1,30 +1,116 @@
 @extends('layouts/contentNavbarLayout')
 
-@section('title', 'Users')
+@section('title', 'Profile')
 
 @section('content')
 <div class="row">
-  <div class="col-12 col-lg-8 order-2 order-md-3 order-lg-2 mb-4">
-    <div class="card">
-      <div class="row row-bordered g-0">
-        <div class="col-md-6">
-           <div class="card-body">
-           <div class="container mt-4">
-            <h1>Show user</h1>
-            <div class="lead"> </div>
-            <div class="container mt-4">
-                <div> Name: {{ $account->name }} </div>
-                <div> Email: {{ $account->email }} </div>
-                <div> Username: {{ $account->username }} </div>
+<div class="col-16 col-lg-12 order-2 order-md-3 order-lg-2 mb-4">
+      <div class="col-md-8">
+        <div class="card mb-3">
+          <div class="card-body">
+          <h4>Company Profile</h4>
+          <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Company Name</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->organisationName }}
+              </div>
             </div>
-
-            <div class="mt-4"> <a href="{{ route('central.accounts.edit', $account) }}" class="btn btn-info">Edit</a> <a
-                    href="{{ route('central.accounts.index') }}" class="btn btn-dark">Back</a> </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Category</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->procurementCategory }}
+              </div>
             </div>
-           </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Description</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->briefDescription }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Phone</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+                {{ $company->companyPhoneNumber }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Country</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->country }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Registration Number</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->registrationNumber }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">TaxID</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->taxId }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Address</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->address }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">City</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->city }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Region</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->region }}
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">Zip Code</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {{ $company->zip_code }}
+              </div>
+            </div>
+            <hr>           
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
+    </div>  
 </div>
 @endsection
